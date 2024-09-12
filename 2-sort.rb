823ed97091078fc9314d22116bd535ccf33fffa3
LS7710 <luissoto7710@gmail.com>
2024-09-12 19:33:57 -0400

    i think its finally fixed -_-

--- 2-sort.rb
+++ 2-sort.rb
@@ -1,31 +1,19 @@
 ###
-#
-#  Sort integer arguments (ascending) 
-#
+#  Sort integer arguments (ascending)
 ###
 
 result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
-    end
-    result << i_arg if !is_inserted
+ARGV.each do |arg|
+  # Skip if not an integer
+  next unless arg =~ /^-?[0-9]+$/
+
+  # Convert to integer and add to result
+  result << arg.to_i
 end
 
+# Sort the result array in ascending order
+result.sort!
+
+# Print the sorted result
 puts result
